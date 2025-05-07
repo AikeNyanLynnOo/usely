3b9a484cc7c2471c3cba5fc46e4a8353d5addadf
a1k3 <66822412+AikeNyanLynnOo@users.noreply.github.com>
2025-05-07 21:52:04 +0700

    first commit, setting up the library, add first hook useDebounce

---
+++ rollup.config.cjs
@@ -0,0 +1,22 @@
+const typescript = require("rollup-plugin-typescript2");
+const commonjs = require("@rollup/plugin-commonjs");
+const resolve = require("@rollup/plugin-node-resolve");
+const pkg = require("./package.json");
+
+module.exports = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main, // CommonJS
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module, // ESM
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+  external: ["react", "react-dom"],
+};
